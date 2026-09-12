65e91230b8b134a23aa92a290ce8231c0c5a1f49
Solzic0 <solzic0@yandex.com>
2026-09-12 13:25:54 +0300

    New Update

--- src-tauri/src/main.rs
+++
@@ -1,8 +0,0 @@
-// LupuS Software Center - Main entry point
-
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    let minimized = std::env::args().any(|a| a == "--minimized");
-    lupus_software_center_lib::run(minimized);
-}
